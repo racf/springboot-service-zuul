302912e880b5e41931bd23237d2276a1040da466
fernando.ramirez <fernando.ramirez@infotec.mx>
2021-05-26 18:04:38 -0500

    first commit

---
+++ src/test/java/com/cysout/springboot/app/zuul/SpringbootServicioZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cysout.springboot.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
